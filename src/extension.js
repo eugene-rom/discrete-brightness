e6014822990f935ae409590367c7832d2a84f43e
Eugene R <eros2@inbox.ru>
2018-05-03 20:20:16 +0300

    get max brightness steps from gsd

--- src/extension.js
+++ src/extension.js
@@ -4,9 +4,9 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const GLib = imports.gi.GLib;
 
 const NUM_MAX      = 15;
-const PRC_MULT     = 100 / NUM_MAX;
 const TEXT_DBRGHNS = 'Discrete brightness';
 const TEXT_LOGID   = 'discrete-brightness';
 
@@ -18,6 +18,9 @@ class DBrightness extends PanelMenu.Button
     constructor() {
         super( 0.0, TEXT_DBRGHNS );
 
+        this._max_br   = this._get_max_brightness();
+        this._prc_mult = 100 / this._max_br;
+
         this._radioGroup = [];
         let hbox = new St.BoxLayout( { style_class: 'panel-status-menu-box' } );
         hbox.add_child( new St.Icon( { icon_name: 'display-brightness-symbolic',
@@ -25,7 +28,7 @@ class DBrightness extends PanelMenu.Button
         this.actor.add_child(hbox);
         this.menu.actor.connect('notify::visible', this._onVisibilityChanged.bind(this));
 
-        for ( var i = NUM_MAX; i >= 0; i-- )
+        for ( var i = this._max_br; i >= 0; i-- )
         {
             let item = new PopupMenu.PopupMenuItem( i.toString() );
             this._radioGroup.push(item);
@@ -36,7 +39,7 @@ class DBrightness extends PanelMenu.Button
                 for ( let i = 0; i < group.length; i++ ) {
                     group[i].setOrnament( (group[i] === item) ? PopupMenu.Ornament.DOT : PopupMenu.Ornament.NONE );
                 }
-                _set_brightness( item.label.get_text() );
+                this._set_brightness( item.label.get_text() );
             });
 
             this.menu.addMenuItem( item );
@@ -47,7 +50,7 @@ class DBrightness extends PanelMenu.Button
     {
         if ( this.menu.actor.visible )
         {
-            let curBr = _get_brightness();
+            let curBr = this._get_brightness();
 
             for ( var i = 0; i < this._radioGroup.length; i++ ) {
                 let b = ( parseInt( this._radioGroup[i].label.get_text() ) === curBr );
@@ -55,16 +58,33 @@ class DBrightness extends PanelMenu.Button
             }
         }
     }
+
+    _get_brightness() {
+        let curBrPercent = Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness;
+        return Math.round( curBrPercent / this._prc_mult );
+    }
+
+    _set_brightness( num ) {
+        Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness = Math.round( num * this._prc_mult );
+    }
+
+    _get_max_brightness()
+    {
+        let maxBr;
+        try {
+            let cmd = '/usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-max-brightness';
+            maxBr = parseInt( String( GLib.spawn_command_line_sync( cmd )[1] ) );
+            if ( maxBr > NUM_MAX ) {
+                maxBr = NUM_MAX;
+            }
+        }
+        catch (err) {
+            maxBr = NUM_MAX;
+        }
+
+        return maxBr;
+    }
 };
-
-function _get_brightness() {
-    let curBrPercent = Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness;
-    return Math.round( curBrPercent / PRC_MULT );
-}
-
-function _set_brightness( num ) {
-    Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness = Math.round( num * PRC_MULT );
-}
 
 
 function enable() {
