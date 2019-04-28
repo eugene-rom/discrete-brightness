8e0f3769376a12cf198020168f4640ed01a1fe58
Eugene R. <eros2@inbox.ru>
2019-04-28 13:40:17 +0300

    gnome shell 3.32 compatibility

--- src/extension.js
+++ src/extension.js
@@ -1,24 +1,23 @@
 /* global imports */
 
-const St = imports.gi.St;
+const { GLib, St } = imports.gi;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
-const GLib = imports.gi.GLib;
 
 const NUM_MAX      = 15;
 const TEXT_DBRGHNS = 'Discrete brightness';
 const TEXT_LOGID   = 'discrete-brightness';
 
 let dbrightness;
-let enabled = false;
 
 class DBrightness extends PanelMenu.Button
 {
-    constructor() {
+    constructor()
+    {
         super( 0.0, TEXT_DBRGHNS );
 
-        this._max_br   = this._get_max_brightness();
+        this._max_br   = _get_max_brightness();
         this._prc_mult = 100 / this._max_br;
 
         this._radioGroup = [];
@@ -26,7 +25,6 @@ class DBrightness extends PanelMenu.Button
         hbox.add_child( new St.Icon( { icon_name: 'display-brightness-symbolic',
                                        style_class: 'system-status-icon' } ) );
         this.actor.add_child(hbox);
-        this.menu.actor.connect('notify::visible', this._onVisibilityChanged.bind(this));
 
         for ( var i = this._max_br; i >= 0; i-- )
         {
@@ -44,56 +42,53 @@ class DBrightness extends PanelMenu.Button
 
             this.menu.addMenuItem( item );
         }
-    }
 
-    _onVisibilityChanged()
-    {
-        if ( this.menu.actor.visible )
-        {
-            let curBr = this._get_brightness();
+        this._onVisibilityChanged = () => {
+            if ( this.menu.actor.visible ) {
+                let curBr = this._get_brightness();
 
-            for ( var i = 0; i < this._radioGroup.length; i++ ) {
-                let b = ( parseInt( this._radioGroup[i].label.get_text() ) === curBr );
-                this._radioGroup[i].setOrnament( b ? PopupMenu.Ornament.DOT : PopupMenu.Ornament.NONE );
+                for ( var i = 0; i < this._radioGroup.length; i++ ) {
+                    let b = ( parseInt( this._radioGroup[i].label.get_text() ) === curBr );
+                    this._radioGroup[i].setOrnament( b ? PopupMenu.Ornament.DOT : PopupMenu.Ornament.NONE );
+                }
             }
-        }
-    }
+        };
 
-    _get_brightness() {
-        let curBrPercent = Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness;
-        return Math.round( curBrPercent / this._prc_mult );
-    }
+        this._get_brightness = () => {
+            let curBrPercent = Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness;
+            return Math.round( curBrPercent / this._prc_mult );
+        };
 
-    _set_brightness( num ) {
-        Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness = Math.round( num * this._prc_mult );
-    }
+        this._set_brightness = ( num ) => {
+            Main.panel.statusArea.aggregateMenu._brightness._proxy.Brightness = Math.round( num * this._prc_mult );
+        };
 
-    _get_max_brightness()
-    {
-        let maxBr;
-        try {
-            let cmd = '/usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-max-brightness';
-            maxBr = parseInt( String( GLib.spawn_command_line_sync( cmd )[1] ) );
-            if ( maxBr > NUM_MAX ) {
-                maxBr = NUM_MAX;
-            }
-        }
-        catch (err) {
-            maxBr = NUM_MAX;
-        }
-
-        return maxBr;
+        this.menu.actor.connect( 'notify::visible', this._onVisibilityChanged.bind(this) );
     }
 };
 
+function _get_max_brightness()
+{
+    let maxBr;
+    try {
+        let cmd = '/usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-max-brightness';
+        maxBr = parseInt( String( GLib.spawn_command_line_sync( cmd )[1] ) );
+        if ( maxBr > NUM_MAX || isNaN( maxBr ) ) {
+            maxBr = NUM_MAX;
+        }
+    }
+    catch (err) {
+        maxBr = NUM_MAX;
+    }
+
+    return maxBr;
+}
 
 function enable() {
-    enabled = true;
     dbrightness = new DBrightness;
     Main.panel.addToStatusArea( TEXT_DBRGHNS, dbrightness );
 }
 
 function disable() {
-    enabled = false;
     dbrightness.destroy();
 }
